bbf620b48c0ae36c793180c9e88d063152e71db8
George <giorgi.bibilashvili89@gmail.com>
2020-10-17 16:31:25 +0400

    Simple Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter App', home: MyHomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Car', amount: 10.50, date: DateTime.now()),
+    Transaction(id: 't2', title: 'New Dog', amount: 34.45, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter App')),
-      body: Center(
-        child: Text('Widget Playground'),
-      ),
-    );
+        appBar: AppBar(title: Text('Flutter App')),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Chart'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: [
+                ...transactions.map((transaction) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                          width: 80,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.purple, width: 2)),
+                          child: Text(transaction.amount.toString(),
+                              style: TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18))),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transaction.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                            ),
+                            Text(transaction.date.toString(),
+                                style: TextStyle(color: Colors.grey))
+                          ])
+                    ],
+                  ));
+                })
+              ],
+            )
+          ],
+        ));
   }
 }
